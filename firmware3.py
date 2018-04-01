62db2ee40d9ac00ea2e42b27767340ad0a7e1bca
Tech Fan <37784171+vpnaccess2015@users.noreply.github.com>
2018-04-01 19:04:06 -0400

    Add files via upload
    Firmware updated to reboot via the STB IP, replace the IP with yours and adjust the sleep timer to match your HR.  Adjust the time it takes from sending the reboot via IP till the first screen shows up to time sending the 02468 etc.

---
+++ firmware3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#from lirc.lirc import Lirc 
+#lircParse = Lirc('/etc/lirc/lircd.conf') 
+#
+#import subprocess
+#rtn = subprocess.call(["irsend", "SEND_ONCE", "rc73", "KEY_mute"])
+# rtn should equal 0 if command ran without error SEND_ONCE rc73 KEY_MUTE
+import sys
+import os
+import time
+import requests
+
+#Send reboot command via Web
+Print 'reboot process startin via Web' 
+ip = '192.168.1.46'
+cmd = r'http://{:s}:8080/serial/processCommand?cmd=FA96'.format(ip).encode('utf-8')
+response =  requests.get(cmd)
+time.sleep(7.2)
+ 
+print ("Now to send update code")
+#upgrade firmware
+#os.system('irsend SEND_ONCE rc73 KEY_0')
+time.sleep(0.5)
+print"0"
+#os.system('irsend SEND_ONCE rc73 KEY_2')
+time.sleep(0.5)
+print"2"
+#os.system('irsend SEND_ONCE rc73 KEY_4')
+time.sleep(0.5)
+print"4"
+#os.system('irsend SEND_ONCE rc73 KEY_6')
+time.sleep(0.5)
+print"6"
+#os.system('irsend SEND_ONCE rc73 KEY_8')
+time.sleep(0.5)
+print"8"
+#Finshed with Auto Update process, will about 10 minutes to reboot
+print "update done"  
